f0584c6601b13960faaaab2b3b50d7fcd277a932
PSEERVERANCE\晒sensei的suputer <54572905@qq.com>
2023-11-26 10:56:21 +0800

    第六章 HTTP请求

--- 7_channels/main.go
+++ 7_channels/main.go
@@ -14,17 +14,27 @@ func main() {
 		"https://www.iqiyi.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
+		//fmt.Println(<-c)
 	}
+
+	for {
+		go checkLink(<-c, c)
+	}
+
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link + "might be down!")
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- link
 }
